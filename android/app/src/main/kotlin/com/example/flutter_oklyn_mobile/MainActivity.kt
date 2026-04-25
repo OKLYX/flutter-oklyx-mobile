63a373750e0c1e7b457da70fa935f753aa39e99e
ydmins <dr.ydmins@gmail.com>
2026-04-25 14:59:58 +0900

    init the project

---
+++ android/app/src/main/kotlin/com/example/flutter_oklyn_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_oklyn_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Oklyn Mobile',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      useMaterial3: true,
+    ),
+    home: const Scaffold(
+      body: Center(
+        child: Text('Flutter Oklyn Mobile'),
+      ),
+    ),
+  );
+}
